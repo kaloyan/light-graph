5881daa977c74eac21ebe5640a7f8c6c2e589f98
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-06-03 14:35:28 +0300

    Simple implementations of Graph class

--- src/index.js
+++ src/index.js
@@ -3,8 +3,46 @@
  * Copyright Kaloyan Ivanov
  */
 
-export class LightGraph {
-  constructor(options = {}) {
-    //todo
+export default class LightGraph {
+  constructor() {
+    this.graph = {};
+    this.routes = [];
+  }
+
+  addNode(node) {
+    this.graph[node] = [];
+  }
+
+  addEdge(node, edge) {
+    this.graph[node].push(edge);
+  }
+
+  print() {
+    console.log(this.graph);
+  }
+
+  traverse(startNode = "") {
+    const routes = [];
+
+    const findRoute = (node, visited = []) => {
+      visited.push(node);
+
+      for (let edge of this.graph[node]) {
+        if (!visited.includes(edge)) {
+          findRoute(edge, [...visited]);
+        } else if (edge === startNode) {
+          if (visited.length > 2 && visited.length < 4) {
+            routes.push([...visited, edge]);
+            // console.log(visited.join(' -> '));
+          }
+        }
+      }
+    };
+
+    findRoute(startNode, []);
+    routes.sort((a, b) => a.length - b.length);
+    this.routes = [...routes];
   }
 }
+
+// module.exports = LightGraph;
